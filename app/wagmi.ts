628c0b09da39a2ec2bb8e3cbfc74ce7775b06471
chiranthamakalanda <makalanda2002@gmail.com>
2026-05-03 03:48:32 +0530

    day 01

---
+++ app/wagmi.ts
@@ -0,0 +1,25 @@
+// app/wagmi.ts
+import { http, createConfig, createStorage, cookieStorage } from 'wagmi';
+import { base } from 'wagmi/chains';
+import { baseAccount, injected } from 'wagmi/connectors';
+
+export const config = createConfig({
+  chains: [base],
+  connectors: [
+    injected(),
+    baseAccount({
+      appName: 'Snake Game',   
+    }),
+  ],
+  storage: createStorage({ storage: cookieStorage }),
+  ssr: true,
+  transports: {
+    [base.id]: http(),
+  },
+});
+
+declare module 'wagmi' {
+  interface Register {
+    config: typeof config;
+  }
+}
