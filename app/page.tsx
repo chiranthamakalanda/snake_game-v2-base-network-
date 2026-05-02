3775efdb08a001e1495005232926ffd6da27890d
chiranthamakalanda <makalanda2002@gmail.com>
2026-05-03 05:00:06 +0530

    fff

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <iframe 
-      src="/index.html" 
-      style={{ 
-        width: '100%', 
-        height: '100vh', 
-        border: 'none' 
-      }}
-      title="Snake Game"
-    />
+    <html lang="en">
+      <head>
+        <meta name="base:app_id" content="69f6736de5227b2ca7cbabad" />
+      </head>
+      <iframe
+        src="/index.html"
+        style={{
+          width: "100%",
+          height: "100vh",
+          border: "none",
+        }}
+        title="Snake Game"
+      />
+    </html>
   );
 }
